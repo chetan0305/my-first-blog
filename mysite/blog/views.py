8bc253b5b81ec4ad47cc0a7b0d752d276d61d56c
chetan0305 <chetanpawar0305@gmail.com>
2019-08-11 17:24:58 +0200

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 # Create your views here.
@@ -8,3 +8,9 @@ def post_list(request):
     posts = Post.objects.all()
     # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk) # gives readable error page if object doesnt exist
+    # post = Post.objects.get(pk=pk) # old way to write above, does not give good error page if object doesnt exist
+    return render(request, 'blog/post_detail.html', {'post': post})
